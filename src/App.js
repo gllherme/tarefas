e89a58131e037d834a463340b006f7054078ebaf
Guilherme Campos <53663956+Tuccano@users.noreply.github.com>
2021-10-04 15:03:00 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
     <div className="container">
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show')}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('Nenhuma tarefa')}
     </div>
   );
 }
